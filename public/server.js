fbf313d273ccc426c3b709bbc5fd6dd4181e7791
szymonpl200 <deadriper.piotrowki@gmail.com>
2020-03-25 19:12:44 +0000

    Integracja z serwerem

--- public/server.js
+++ public/server.js
@@ -1,7 +1,8 @@
+
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
-console.log('@@@MAGAR'+__dirname+'/public');
+
 app.use(express.static(__dirname + '/public'));
 
 var server = app.listen(port);
